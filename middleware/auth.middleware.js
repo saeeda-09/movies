2232a02e4a6f20a0584f8e03cbd36d445bf62736
saeeda-09 <saeedamuhtashima@gmail.com>
2026-01-02 20:15:09 +0600

    middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model.js');
+
+const protect = async (req, res, next) => {
+    let token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ message: "Not authorized, no token" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.userId).select('-password');
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Not authorized, token failed" });
+    }
+};
+
+const admin = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(403).json({ message: "Not authorized as an admin" });
+    }
+};
+
+module.exports = { protect, admin };
